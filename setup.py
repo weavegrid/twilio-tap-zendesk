7f567b58d3188b667be1aebe0ed9c7fa56f7c482
Nick Clarke <nickolasclarke@gmail.com>
2024-07-10 13:35:50 -0600

    sync with master, but re-pin requests back to .31

--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@
 from setuptools import setup
 
 setup(
-      name='twilio-tap-zendesk',
-      version='1.0.17',
-      description='Singer.io tap for extracting data from the Zendesk API',
-      author='Twilio',
-      url='https://github.com/twilio-labs/twilio-tap-zendesk',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_zendesk'],
-      install_requires=[
-          'pipelinewise-singer-python==1.2.0',
-          'zenpy==2.0.49',
-          'requests==2.20.0'
-      ],
-      extras_require={
-          'dev': [
-              'ipdb',
-              'pylint',
-              'nose',
-              'nose-watch',
-          ]
-      },
-      entry_points='''
+    name="twilio-tap-zendesk",
+    version="1.0.17",
+    description="Singer.io tap for extracting data from the Zendesk API",
+    author="Twilio",
+    url="https://github.com/twilio-labs/twilio-tap-zendesk",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_zendesk"],
+    install_requires=[
+        "pipelinewise-singer-python==1.2.0",
+        "zenpy==2.0.49",
+        "requests==2.31.0",
+    ],
+    extras_require={
+        "dev": [
+            "ipdb",
+            "pylint",
+            "nose",
+            "nose-watch",
+        ]
+    },
+    entry_points="""
           [console_scripts]
           tap-zendesk=tap_zendesk:main
-      ''',
-      packages=['tap_zendesk'],
-      include_package_data=True,
+      """,
+    packages=["tap_zendesk"],
+    include_package_data=True,
 )
